abbf27760fd9648ad837b58649cbf6d0eead0d12
Hush <88868365+hushgames@users.noreply.github.com>
2024-09-11 16:37:30 +0800

    Create data.py

---
+++ data.py
@@ -0,0 +1,10 @@
+'''
+	data container
+'''
+slist:list = [
+	{'idno':'0001','lastname':'alpha','firstname':'foxtrot','course':'bsit','level':'2',},
+	{'idno':'0002','lastname':'golf','firstname':'echo','course':'bscs','level':'4',},
+	{'idno':'0003','lastname':'hotel','firstname':'delta','course':'bscream','level':'2',},
+	{'idno':'0004','lastname':'india','firstname':'charlie','course':'bshm','level':'3',},
+	{'idno':'0005','lastname':'juliet','firstname':'bravo','course':'bsba','level':'1',},
+]
